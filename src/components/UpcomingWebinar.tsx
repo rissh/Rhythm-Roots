65ffce7ab52800c0e36fa6341f3665810956a782
Rishikesh <rishikeshjagadale19@gmail.com>
2024-06-02 15:37:16 +0530

    Enhance Button Styling in Upcoming Webinar Section
    - Updated the button styling in the UpcomingWebinar component for better UI/UX.

--- src/components/UpcomingWebinar.tsx
+++ src/components/UpcomingWebinar.tsx
@@ -92,11 +92,13 @@ function UpcomingWebinar() {
         </div>
 
         <div className="mt-10 text-center">
-          <Link
-            href={"/"}
-            className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
-          >
-            View All webinars
+          <Link href={"#"}>
+            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                View All Courses
+              </span>
+            </button>
           </Link>
         </div>
       </div>
